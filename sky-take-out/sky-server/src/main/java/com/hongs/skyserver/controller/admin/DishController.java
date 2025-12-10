d19043096f93b7bfcc337bee5e2761700eb963d0
Hongs_Cai <Hongs_Cai@outlook.com>
2025-12-10 23:34:26 +0800

    refactor(dish): 移除循环依赖注入
    - 删除了 DishServiceImpl 中对自身服务的不必要注入
    - 确保依赖关系清晰且无循环引用
    - 提高代码可维护性和测试性

--- sky-take-out/sky-server/src/main/java/com/hongs/skyserver/controller/admin/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/hongs/skyserver/controller/admin/DishController.java
@@ -3,9 +3,12 @@ package com.hongs.skyserver.controller.admin;
 
 import com.hongs.skycommon.pojo.dto.DishSaveDTO;
 import com.hongs.skycommon.result.Result;
+import com.hongs.skyserver.service.DishService;
+import com.hongs.skyserver.service.impl.DishServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "菜品相关接口")
 public class DishController {
 
+    @Autowired
+    private DishService dishService;
+
     /**
      * 新增菜品
      * @param dishSaveDTO
@@ -29,8 +35,7 @@ public class DishController {
     @PostMapping
     public Result save(@RequestBody DishSaveDTO dishSaveDTO) {
         log.info("新增菜品: {}", dishSaveDTO);
-
-
+        dishService.saveWithFlavor(dishSaveDTO);
         return Result.success();
     }
 
