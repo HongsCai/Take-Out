29cb4df3a7b6f62081ea10a20b4811ed960efd10
Hongs_Cai <Hongs_Cai@outlook.com>
2025-12-20 23:59:11 +0800

    feat(index): 更新首页模块功能
    - 修改 index.js 文件逻辑
    - 更新 index.wxml 页面结构
    - 完善学习记录文档内容

--- sky-take-out-miniprogram/pages/index/index.js
+++ sky-take-out-miniprogram/pages/index/index.js
@@ -1,4 +1,16 @@
 // index.js
 Page({
-    
+  data: {
+    msg: "Hongs_Cai"
+  },
+
+  // 获取微信头像和昵称
+  getUserInfo() {
+    wx.getUserProfile({
+      desc: '获取用户信息',
+      success: (res) => {
+        console.log(res.userInfo)
+      }
+    })
+  }
 })
